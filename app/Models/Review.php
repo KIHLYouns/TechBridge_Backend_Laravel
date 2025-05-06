5c19794371e42756da29cc370d93396be1cf9975
aya2705 <touicha.aya@gmail.com>
2025-05-06 16:33:42 +0100

    Merge branch 'feature/Gestion_utilisateur référence' into sprint1_merged

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -6,4 +7,43 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Review extends Model
 {
+    use HasFactory;
+
+    protected $table = 'review';
+
+    protected $fillable = [
+        'reservation_id',
+        'rating',
+        'comment',
+        'is_visible',
+        'created_at',
+        'type',
+        'reviewer_id',
+        'reviewee_id',
+        'listing_id',
+    ];
+
+    public $timestamps = false;
+
+    // Relations
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
+
+    public function reviewer()
+    {
+        return $this->belongsTo(User::class, 'reviewer_id');
+    }
+
+    public function reviewee()
+    {
+        return $this->belongsTo(User::class, 'reviewee_id');
+    }
+
+    public function listing()
+    {
+        return $this->belongsTo(Listing::class);
+    }
 }
